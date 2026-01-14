7e21225384a2c01736fb2eb70a9e42cdcd42b7e9
cavalomascaku-ui <cavalomascaku@gmail.com>
2026-01-14 16:06:52 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
-self.addEventListener('install', e => {
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// <<< ESSENCIAL PRA INSTALAÇÃO >>>
+self.addEventListener('fetch', event => {
+  // Esse handler faz com que o SW *controle* a página
+  event.respondWith(fetch(event.request));
 });
